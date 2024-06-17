ce01651842d9e9a984a7e9e63c9f95aeb986a3d8
KeJun11 <144515947+KeJun11@users.noreply.github.com>
2024-06-17 17:41:43 +0800

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,12 @@
 import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 
+const apiUrl = "/c20db853-21fe-4f11-b230-00ff84048af0-dev.e1-us-east-azure.choreoapis.dev/djangoreact/backend/v1.0"
+
 const api = axios.create({
     // allows us to import anything from the env file
     // basically links react to the backend server
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 })
 
 api.interceptors.request.use(
